90ce9d16bc1107f8246c9eb832e05dfe4f9c3978
evasyukov <egor.vasyukov1@gmail.com>
2024-02-07 13:54:03 +0700

    add calculator buttons

--- src/components/app/AppFour.jsx
+++ src/components/app/AppFour.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react"
+import Button from "../Button"
+
+let calcButton = [
+    { val: "1" },
+    { val: "2" },
+    { val: "3" },
+    { val: "4" },
+    { val: "5" },
+    { val: "6" },
+    { val: "7" },
+    { val: "8" },
+    { val: "9" },
+    { val: "0" },
+    { val: "=" },
+    { val: "+" },
+    { val: "-" },
+    { val: "*" },
+    { val: "/" }
+]
+
 export default function AppFour() {
-    return (
-        <h3>калькулятор</h3>
-    )
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>калькулятор</h3>
+      <input
+        style={{ marginBottom: "1rem" }}
+        defaultValue={count}
+        type="text"
+        id="calc"
+      />
+      <div>
+        {calcButton.map((item) => (
+          <Button key={item}>{item.val}</Button>
+        ))}
+      </div>
+    </>
+  )
 }
